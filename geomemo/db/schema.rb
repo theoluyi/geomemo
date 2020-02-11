d0d152efb45faaaef7fb83e9f357c8befb4582eb
Lori Boyd <lori.p.boyd@gmail.com>
2020-02-11 10:19:55 -0500

    Added seed data

---
+++ geomemo/db/schema.rb
@@ -0,0 +1,41 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_02_10_225859) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "notebooks", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.boolean "nsfw"
+    t.integer "notebook_id", null: false
+    t.integer "location_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["location_id"], name: "index_notes_on_location_id"
+    t.index ["notebook_id"], name: "index_notes_on_notebook_id"
+  end
+
+  add_foreign_key "notes", "locations"
+  add_foreign_key "notes", "notebooks"
+end
--- geomemo/db/seeds.rb
+++ geomemo/db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+def string_for_guidelines
+  "
+  Add controllers to the app.
+  Migrate models.
+  Add seed data.
+  Rake db:seed.
+  Create views
+  "
+end
+
+Note.destroy_all
+Notebook.destroy_all
+Location.destroy_all
+
+Notebook.create(name: "Personal")
+Notebook.create(name: "Work")
+
+Location.create(name: "Kitchen")
+Location.create(name: "Bedside Table")
+Location.create(name: "School")
+Location.create(name: "Back Pocket")
+Location.create(name: "Bank")
+Location.create(name: "Car")
+Location.create(name: "Laptop")
+
+Note.create(title: "Grocery List", content: "General Tso's Chicken", nsfw: false, notebook_id: Notebook.find_by(name: "Personal").id, location_id: Location.find_by(name: "Kitchen").id)
+Note.create(content: "Birthday card for bro", nsfw: false, notebook_id: Notebook.find_by(name: "Personal").id, location_id: Location.find_by(name: "Back Pocket").id)
+Note.create(title: "Project Guidelines", content: string_for_guidelines, nsfw: false, notebook_id: Notebook.find_by(name: "Work").id, location_id: Location.find_by(name: "Laptop").id)
+
+def string_for_guidelines
+  "
+  Add controllers to the app.
+  Migrate models.
+  Add seed data.
+  Rake db:seed.
+  Create views
+  "
+end
